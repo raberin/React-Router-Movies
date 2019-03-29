95384b986f50eff0c4946248f33f8b8705765cf9
raberin <rmvaberin@gmail.com>
2019-03-28 21:00:33 -0700

    Moved the code in the MovieDetails function in to the MovieCard component to make it more reusable and DRY

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 
+import MovieCard from "./MovieCard";
+
 export default class MovieList extends Component {
   constructor(props) {
     super(props);
@@ -28,32 +30,10 @@ export default class MovieList extends Component {
           // This "Link" tag is used to wrap around each movie item. The 'to' is basically the href in a tag.
           //Used template literals to add in the id so that i has access all id in the list. Makes the id dynamic.
           <Link to={`/movies/${movie.id}`}>
-            <MovieDetails key={movie.id} movie={movie} />
+            <MovieCard key={movie.id} movie={movie} />
           </Link>
         ))}
       </div>
     );
   }
-}
-
-function MovieDetails({ movie }) {
-  const { title, director, metascore, stars } = movie;
-  return (
-    <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-      <h3>Actors</h3>
-
-      {stars.map(star => (
-        <div key={star} className="movie-star">
-          {star}
-        </div>
-      ))}
-    </div>
-  );
 }
